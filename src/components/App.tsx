bc0700651796fd48ae31b402a5e0c7a770f27e93
salikasl <salikasl@umich.edu>
2021-09-05 23:28:38 -0400

    fetching kinda works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,11 +6,8 @@ import { Query } from '../types/games'
 
 function App() {
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault()
-
-  }
-
+  const [username, setUsername] = useState<string>("")
+  
   const [filters, setFilters] = useState<Query>({
     smothered: false,
     enpassant: false,
@@ -18,26 +15,53 @@ function App() {
     stalemate: false,
     castles: false
   })
+  
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    const url = `https://api.chess.com/pub/player/${username}/games/archives`
 
+    const data = fetch(url)
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json()
+      }).then(data => console.log(data))
+
+  }
+
+  
   return (
     <div className="App">
+      <h1>Chess Search</h1>
       <div className="searchBox">
-        <form action="">
-          <label> Smothered checkmate
-            <input type="checkbox" name="smothered"/>
-          </label>
-          <label> Stalemate
-            <input type="checkbox" name="stalemate"/>
-          </label>
-          <label> Pawn checkmate
-            <input type="checkbox" name="pawn"/>
-          </label>
-          <label> Castles checkmate
-            <input type="checkbox" name="castles"/>
-          </label>
-          <label> En Passant checkmate
-            <input type="checkbox" name="enpassant"/>
-          </label>
+        <form onSubmit={handleSubmit}>
+          
+          <h1>Enter your username</h1>
+          <input className="textBox" type="text" placeholder="chess.com username" onChange={(e: any) => (setUsername(e.target.value))}/>
+
+          <h1>Filters</h1>
+          <div className="checksContainer">
+            <label>
+              <input type="checkbox" name="smothered"/>
+              Smothered checkmate
+            </label>
+            <label> 
+              <input type="checkbox" name="stalemate"/>
+              Stalemate
+            </label>
+            <label> 
+              <input type="checkbox" name="pawn"/>
+              Pawn checkmate
+            </label>
+            <label>
+              <input type="checkbox" name="castles"/>
+              Castles checkmate
+            </label>
+            <label>
+              <input type="checkbox" name="enpassant"/>
+              En Passant checkmate
+            </label>
+          </div>
+
           <input type="submit" value="Search Games" />
         </form>
       </div>
